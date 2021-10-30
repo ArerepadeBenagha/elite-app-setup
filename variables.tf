ef46c49b4587d61b462fe53eb157306393c8e3ff
ArerepadeB <aredelulu@gmail.com>
2021-10-30 01:30:14 -0500

    tfc test

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {
-  default = "us-east-1"
-}
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
+# variable "aws_region" {
+#   default = "us-east-1"
+# }
 variable "path_to_public_key" {
   description = "public key"
   default     = "ngnixkey.pub"
